c438fbbcbcd81ac4044408357856fa1dcb76ae98
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-22 18:12:58 +0530

    Converting to sha1 hash

--- password_safety_checker.py
+++ password_safety_checker.py
@@ -1,5 +1,5 @@
 import requests
-
+import hashlib
 password_api_url = "https://api.pwnedpasswords.com/range/" + "password123"
 res = requests.get(password_api_url)
 print(res)
@@ -12,7 +12,12 @@ def request_api_data(query_char):
     url = "https://api.pwnedpasswords.com/range/" + query_char
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError('Error fetching: {}')
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
 
 def pwned_api_check(password):
-    pass
+    # check if our password in the returned list
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
+# request_api_data('123')
+print(pwned_api_check('123'))
