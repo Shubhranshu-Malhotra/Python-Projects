ccd890435f55fcd0cf860e51c93b51802a26209f
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-22 18:03:09 +0530

    structure of password checker

--- password_safety_checker.py
+++ password_safety_checker.py
@@ -3,3 +3,13 @@ import requests
 password_api_url = "https://api.pwnedpasswords.com/range/" + "password123"
 res = requests.get(password_api_url)
 print(res)
+
+password_api_url = "https://api.pwnedpasswords.com/range/" + "CBFDA"   # starting letters of hashcode
+res = requests.get(password_api_url)
+print(res)
+
+def request_api_data(query_char):
+    pass
+
+def pwned_api_check(password):
+    pass
